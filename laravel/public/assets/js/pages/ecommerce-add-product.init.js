661c99be984d57a8255e35c4a3348139e753eb89
Klaas Schoute <klaas_schoute@hotmail.com>
2021-07-02 01:48:03 +0200

    Update Bootstrap to latest version

--- laravel/public/assets/js/pages/ecommerce-add-product.init.js
+++ laravel/public/assets/js/pages/ecommerce-add-product.init.js
@@ -1,16 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************************************************!*\
-  !*** ./resources/js/pages/ecommerce-add-product.init.js ***!
-  \**********************************************************/
-/*
-Template Name: Minible - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://themesbrand.com/
-Contact: themesbrand@gmail.com
-File: ecommerce add product Js File
-*/
-// Select2
 $(".select2").select2();
-/******/ })()
-;
--- laravel/public/assets/js/pages/form-mask.init.js
+++ laravel/public/assets/js/pages/form-mask.init.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************************************!*\
-  !*** ./resources/js/pages/form-mask.init.js ***!
-  \**********************************************/
-/*
-Template Name: Minible - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://themesbrand.com/
-Contact: themesbrand@gmail.com
-File: Form mask Js File
-*/
-$(document).ready(function () {
-  $(".input-mask").inputmask();
-});
-/******/ })()
-;
+$(document).ready((function(){$(".input-mask").inputmask()}));
