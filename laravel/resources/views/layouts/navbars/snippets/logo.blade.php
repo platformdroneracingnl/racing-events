797015a997157c64b4b6d5ba947320a04faf026a
Klaas Schoute <klaas_schoute@hotmail.com>
2021-07-03 16:05:29 +0200

    Update logo

--- laravel/resources/views/layouts/navbars/snippets/logo.blade.php
+++ laravel/resources/views/layouts/navbars/snippets/logo.blade.php
@@ -2,19 +2,19 @@
 <div class="navbar-brand-box">
     <a href="{{ route('dashboard')}}" class="logo logo-dark">
         <span class="logo-sm">
-            <img src="{{ URL::asset('/assets/images/logo-sm.png') }}" alt="" height="22">
+            <img src="{{ URL::asset('/pdrnl/img/brand/logo-small.svg') }}" alt="" height="22">
         </span>
         <span class="logo-lg">
-            <img src="{{ URL::asset('/pdrnl/img/brand/pdrnl-black.png') }}" alt="" height="25">
+            <img src="{{ URL::asset('/pdrnl/img/brand/logo-dark.svg') }}" alt="" height="25">
         </span>
     </a>
 
     <a href="{{ route('dashboard')}}" class="logo logo-light">
         <span class="logo-sm">
-            <img src="{{ URL::asset('/assets/images/logo-sm.png') }}" alt="" height="22">
+            <img src="{{ URL::asset('/pdrnl/img/brand/logo-small.svg') }}" alt="" height="22">
         </span>
         <span class="logo-lg">
-            <img src="{{ URL::asset('/pdrnl/img/brand/pdrnl-white.png') }}" alt="" height="25">
+            <img src="{{ URL::asset('/pdrnl/img/brand/logo-white.svg') }}" alt="" height="25">
         </span>
     </a>
 </div>
