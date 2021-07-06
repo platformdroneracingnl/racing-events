646e6acf8b34c5a5aa3860577e1bc65e73bf384a
Klaas Schoute <klaas_schoute@hotmail.com>
2021-07-06 12:14:03 +0200

    User can change profile

--- laravel/resources/views/backend/profile/parts/profile_password.blade.php
+++ laravel/resources/views/backend/profile/parts/profile_password.blade.php
@@ -15,29 +15,41 @@
     @endif
 
     <div class="pl-lg-4">
-        <div class="mb-3 {{ $errors->has('old_password') ? ' has-danger' : '' }}">
-            <label class="form-control-label" for="input-current-password">{{ __('Current Password') }}</label>
-            <input type="password" name="old_password" id="input-current-password" class="form-control form-control-alternative{{ $errors->has('old_password') ? ' is-invalid' : '' }}" placeholder="{{ __('Current Password') }}" value="" required>
-            
-            @if ($errors->has('old_password'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('old_password') }}</strong>
-                </span>
-            @endif
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="mb-3 {{ $errors->has('old_password') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="input-current-password">{{ __('Current Password') }}</label>
+                    <input type="password" name="old_password" id="input-current-password" class="form-control {{ $errors->has('old_password') ? ' is-invalid' : '' }}" placeholder="{{ __('Current Password') }}" value="" required>
+                    
+                    @if ($errors->has('old_password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('old_password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
         </div>
-        <div class="mb-3 {{ $errors->has('password') ? ' has-danger' : '' }}">
-            <label class="form-control-label" for="input-password">{{ __('New Password') }}</label>
-            <input type="password" name="password" id="input-password" class="form-control form-control-alternative{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('New Password') }}" value="" required>
-            
-            @if ($errors->has('password'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="mb-3 {{ $errors->has('password') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="input-password">{{ __('New Password') }}</label>
+                    <input type="password" name="password" id="input-password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('New Password') }}" value="" required>
+                    
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label class="form-control-label" for="input-password-confirmation">{{ __('Confirm New Password') }}</label>
-            <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control form-control-alternative" placeholder="{{ __('Confirm New Password') }}" value="" required>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="mb-3">
+                    <label class="form-control-label" for="input-password-confirmation">{{ __('Confirm New Password') }}</label>
+                    <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control" placeholder="{{ __('Confirm New Password') }}" value="" required>
+                </div>
+            </div>
         </div>
 
         <div class="text-center">
