00e955f216b3e032b8bb69915147fd5aec41faf4
Klaas Schoute <klaas_schoute@hotmail.com>
2021-11-18 03:02:32 +0100

    Add some translations

--- laravel/resources/lang/en/pdrnl.php
+++ laravel/resources/lang/en/pdrnl.php
@@ -2,6 +2,9 @@
 
 return [
     'welcome' => 'Welcome to the registration platform of <br>Platform Drone Racing Netherlands',
+    'rights' => 'All rights reserved',
+
+    # Notifications
     'notify_nr_single' => 'You have <strong class="text-primary">:number</strong> notification',
     'notify_nr_multi' => 'You have <strong class="text-primary">:number</strong> notifications',
     'notify_multi' => 'Notifications',
