1cffbd30ce4bfa971440301ff3af2599c7d46e61
Klaas Schoute <klaas_schoute@hotmail.com>
2021-11-24 02:32:38 +0100

    Add latest registrations to dashboard view

--- laravel/resources/views/backend/index.blade.php
+++ laravel/resources/views/backend/index.blade.php
@@ -202,108 +202,51 @@
                             </div>
                         </div>
                     </div>
-                    <h4 class="card-title mb-4">Top Users</h4>
+                    <h4 class="card-title mb-4">Recente Aanmeldingen</h4>
 
                     <div data-simplebar style="max-height: 336px;">
                         <div class="table-responsive">
                             <table class="table table-borderless table-centered table-nowrap">
                                 <tbody>
-                                    <tr>
-                                        <td style="width: 20px;"><img
-                                                src="{{ URL::asset('/assets/images/users/avatar-4.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Glenn Holden</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
-                                                Nevada</p>
-                                        </td>
-                                        <td><span class="badge bg-soft-danger font-size-12">Cancel</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i
-                                                class="icon-xs icon me-2 text-success"
-                                                data-feather="trending-up"></i>$250.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-5.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Lolita Hamill</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i> Texas
-                                            </p>
-                                        </td>
-                                        <td><span class="badge bg-soft-success font-size-12">Success</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i class="icon-xs icon me-2 text-danger"
-                                                data-feather="trending-down"></i>$110.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-6.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Robert Mercer</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
-                                                California</p>
-                                        </td>
-                                        <td><span class="badge bg-soft-info font-size-12">Active</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i
-                                                class="icon-xs icon me-2 text-success"
-                                                data-feather="trending-up"></i>$420.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-7.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Marie Kim</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
-                                                Montana</p>
-                                        </td>
-                                        <td><span class="badge bg-soft-warning font-size-12">Pending</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i class="icon-xs icon me-2 text-danger"
-                                                data-feather="trending-down"></i>$120.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-8.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Sonya Henshaw</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
-                                                Colorado</p>
-                                        </td>
-                                        <td><span class="badge bg-soft-info font-size-12">Active</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i
-                                                class="icon-xs icon me-2 text-success"
-                                                data-feather="trending-up"></i>$112.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-2.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Marie Kim</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
-                                                Australia</p>
-                                        </td>
-                                        <td><span class="badge bg-soft-success font-size-12">Success</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i class="icon-xs icon me-2 text-danger"
-                                                data-feather="trending-down"></i>$120.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="{{ URL::asset('/assets/images/users/avatar-1.jpg') }}"
-                                                class="avatar-xs rounded-circle " alt="..."></td>
-                                        <td>
-                                            <h6 class="font-size-15 mb-1 fw-normal">Sonya Henshaw</h6>
-                                            <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i> India
-                                            </p>
-                                        </td>
-                                        <td><span class="badge bg-soft-danger font-size-12">Cancel</span></td>
-                                        <td class="text-muted fw-semibold text-end"><i
-                                                class="icon-xs icon me-2 text-success"
-                                                data-feather="trending-up"></i>$112.00</td>
-                                    </tr>
+                                    @foreach ($registrations as $registration)
+                                        <tr>
+                                            <td style="width: 20px;"><img
+                                                    src="{{ URL::asset('/assets/images/users/avatar-4.jpg') }}"
+                                                    class="avatar-xs rounded-circle " alt="...">
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('events.show',$registration->event->id) }}">
+                                                    <h6 class="font-size-15 mb-1 fw-normal">{{ $registration->event->name }}</h6>
+                                                </a>
+                                                <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i>
+                                                    {{ $registration->event->location->name }}</p>
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('registrations.index') }}">
+                                                    <h5 class="mb-0">
+                                                        <!-- Registration status -->
+                                                        @include('backend.snippets.registration-status')
+                                                    </h5>
+                                                </a>
+                                            </td>
+                                            <td class="text-muted fw-semibold text-end"><i
+                                                    class="icon-xs icon me-2 text-success"
+                                                    data-feather="trending-up"></i>
+                                                    @if ($registration->event->price == 0)
+                                                        {{ __('Free') }}!
+                                                    @else
+                                                        €{{ number_format($registration->event->price, 2) }}
+                                                    @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
-                        </div> <!-- enbd table-responsive-->
-                    </div> <!-- data-sidebar-->
-                </div><!-- end card-body-->
-            </div> <!-- end card-->
-        </div><!-- end col -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="col-xl-4">
             <div class="card">
