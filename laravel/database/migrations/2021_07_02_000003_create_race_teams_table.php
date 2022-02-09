adc879dbd3f6cb9201b10f39a6da023d12c6bb87
Shift <shift@laravelshift.com>
2022-02-09 13:56:34 +0000

    Adopt anonymous migrations

--- laravel/database/migrations/2021_07_02_000003_create_race_teams_table.php
+++ laravel/database/migrations/2021_07_02_000003_create_race_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRaceteamsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateRaceteamsTable extends Migration
     {
         Schema::dropIfExists('race_teams');
     }
-}
+};
