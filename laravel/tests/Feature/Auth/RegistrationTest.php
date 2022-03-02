b035dbd92ba8579fa577aa8ad88236e81bf414bb
Klaas Schoute <klaas_schoute@hotmail.com>
2022-03-03 00:28:09 +0100

    Add tests for auth

---
+++ laravel/tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_registration_screen_can_be_rendered()
+    {
+        $this->refreshApplicationWithLocale('en');
+
+        $response = $this->get(route('register'));
+        $response->assertStatus(200);
+    }
+}
