94405c716fa3215a4f2b9b77508c248ff8170954
Klaas Schoute <klaas_schoute@hotmail.com>
2022-03-21 23:24:27 +0100

    Put foreign key constraints in new format

--- laravel/database/migrations/2021_07_02_000000_create_countries_table.php
+++ laravel/database/migrations/2021_07_02_000000_create_countries_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->bigIncrements('id');
             $table->string('code');
             $table->string('name');
+            $table->timestamps();
         });
     }
 
--- laravel/database/migrations/2021_07_02_000010_create_event_registrations_table.php
+++ laravel/database/migrations/2021_07_02_000010_create_event_registrations_table.php
@@ -15,24 +15,13 @@ return new class extends Migration {
         Schema::create('event_registrations', function (Blueprint $table) {
             $table->string('reg_id')->primary();
             $table->string('payment_id')->nullable();
-            $table->bigInteger('event_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
-            $table->bigInteger('status_id')->unsigned();
+            // Delete row if specific event is deleted
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('status_id')->constrained('registration_status');
             $table->boolean('failsafe')->default(0);
             $table->boolean('vtx_power')->default(0);
             $table->timestamps();
-
-            $table->foreign('event_id')
-                ->references('id')
-                ->on('events')
-                // Delete row if specific event is deleted
-                ->onDelete('cascade');
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users');
-            $table->foreign('status_id')
-                ->references('id')
-                ->on('registration_status');
         });
     }
 
