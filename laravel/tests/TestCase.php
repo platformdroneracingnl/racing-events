97c142fdf1404188851e627ec08050fdd35d4bcf
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-15 01:10:23 +0100

    Update the tests

--- laravel/tests/TestCase.php
+++ laravel/tests/TestCase.php
@@ -10,17 +10,4 @@ use Mcamara\LaravelLocalization\LaravelLocalization;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, AdditionalAssertions, RefreshDatabase;
-
-    protected function refreshApplicationWithLocale($locale)
-    {
-        self::tearDown();
-        putenv(LaravelLocalization::ENV_ROUTE_KEY . '=' . $locale);
-        self::setUp();
-    }
-
-    protected function tearDown() : void
-    {
-        putenv(LaravelLocalization::ENV_ROUTE_KEY);
-        parent::tearDown();
-    }
 }
