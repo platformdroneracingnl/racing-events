9363de72defbc81e85873f4b8c448713a678ef15
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-15 17:11:57 +0100

    Add tests for raceteams management with policy

--- laravel/database/factories/RaceTeamFactory.php
+++ laravel/database/factories/RaceTeamFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
 use App\Models\RaceTeam;
 
 class RaceTeamFactory extends Factory
@@ -23,6 +25,7 @@ class RaceTeamFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'image' => UploadedFile::fake()->image('avatar.jpg'),
         ];
     }
 }
