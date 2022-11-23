4a4373518db22f166c265f908943fd5defd32c5e
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-24 00:37:04 +0100

    Set validation rules for roles

---
+++ laravel/app/Http/Requests/Management/StoreRoleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Management;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('role-create');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required|array',
+        ];
+    }
+}
---
+++ laravel/app/Http/Requests/Management/UpdateRoleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Management;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('role-update');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:roles,name,' . $this->route('role')->id,
+            'permission' => 'required|array',
+        ];
+    }
+}
