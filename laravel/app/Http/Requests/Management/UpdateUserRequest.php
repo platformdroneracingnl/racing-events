4d242532f9a16b471f79cbfa2d873209835248f5
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-24 01:49:21 +0100

    Add form validation for users (#748)
    * Set validation rules for users
    
    * Show errors on create and edit page for users

---
+++ laravel/app/Http/Requests/Management/UpdateUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Management;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('user-update');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'pilot_name' => 'nullable|string',
+            'email' => 'required|email|unique:users,email,' . $this->route('user')->id,
+            'organization_id' => 'nullable|exists:organizations,id',
+            'race_team_id' => 'nullable|exists:race_teams,id',
+            'password' => 'same:confirm-password',
+            'roles' => 'required',
+        ];
+    }
+}
