86bf97610db14a2ae117041894319864a4216d7f
Klaas Schoute <klaas_schoute@hotmail.com>
2023-01-12 00:43:33 +0100

    Move clockwork package to dev

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('local') && config('clockwork.enable')) {
+            $this->app->register(\Clockwork\Support\Laravel\ClockworkServiceProvider::class);
+        }
     }
 
     /**
