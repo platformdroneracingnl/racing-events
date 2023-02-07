b339fc2ae773bfda6a025e905c0a7ff6e26e35f4
Klaas Schoute <klaas_schoute@hotmail.com>
2023-02-08 00:56:34 +0100

    Update the fullcalendar timegrid url

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -78,11 +78,10 @@ const folder = {
                     "./node_modules/@fullcalendar/core/index.global.js",
                     "./node_modules/@fullcalendar/bootstrap/main.js",
                     "./node_modules/@fullcalendar/daygrid/main.js",
-                    "./node_modules/@fullcalendar/timegrid/main.js",
+                    "./node_modules/@fullcalendar/timegrid/index.global.js",
                     "./node_modules/@fullcalendar/interaction/main.js",
                     "./node_modules/@fullcalendar/bootstrap/main.css",
-                    "./node_modules/@fullcalendar/daygrid/main.css",
-                    "./node_modules/@fullcalendar/timegrid/main.css"]
+                    "./node_modules/@fullcalendar/daygrid/main.css"]
             },
             {
                 "name": "jquery-vectormap", "assets": ["./node_modules/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js",
